f0042d0e2acb4d86f86842332948b7e943a956bf
John Fanidis <johnfanidis@gmail.com>
2020-12-05 23:33:24 +0000

    Updated card class variable name in main class

--- src/BankingApplication.java
+++ src/BankingApplication.java
@@ -1,7 +1,8 @@
 public class BankingApplication {
 
     public static void main(String[] args) {
-        CreditCard creditCard1 = new CreditCard("John Fanidis", "€");
+        CreditCard nbgCard = new CreditCard("John Fanidis", "€");
+
         ATM atm = new ATM();
         atm.insertCard(creditCard1);
     }
